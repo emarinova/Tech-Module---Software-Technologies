c64919da95b7dc614f197317b84e70f3abb756ab
emarinova <elitsa.marinova90@gmail.com>
2018-03-25 18:23:39 +0300

    C#

---
+++ C#/BookLibrary/BookLibrary/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace BookLibrary.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        
+        //public virtual DbSet<Book> Books { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
